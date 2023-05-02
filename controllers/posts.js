4175f24f15b21ec50a9a77c042570cb5e01e9ab2
CCMatson <catherine.matson@gmail.com>
2023-05-01 19:39:46 -0700

    ake a create post router and controller

---
+++ controllers/posts.js
@@ -0,0 +1,18 @@
+const { Post } = require('../models')
+
+async function create(req, res) {
+  try {
+		req.body.profileId = req.user.profile.id
+    const post = await Post.create(req.body)
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ err: error })
+  }
+}
+
+
+module.exports = {
+  create,
+
+}
